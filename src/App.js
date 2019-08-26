ae6bfd30b84dd30fb47912187be5c4da85e365ac
Raphael Freire <raphaelmachadofreire@gmail.com>
2019-08-25 23:37:10 -0300

    Criado o object input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 
+import Input from "./objects/Input";
 import Label from "./objects/Label";
 import Header from "./components/Header";
 import Hashtag from "./components/Hastag";
@@ -9,6 +10,7 @@ const App = () => (
 	<main className="app">
 		<Header />
 		<Hashtag />
+		<Input value="show" />
 		<Label content="Mostar eventos" />
 	</main>
 );
