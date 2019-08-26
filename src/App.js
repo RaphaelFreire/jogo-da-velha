48ac318274fd1f3e7ae44ea205751c991780b65f
Raphael Freire <raphaelmachadofreire@gmail.com>
2019-08-25 23:31:46 -0300

    Foi criado o object label

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+
+import Label from "./objects/Label";
 import Header from "./components/Header";
 import Hashtag from "./components/Hastag";
 
@@ -7,6 +9,7 @@ const App = () => (
 	<main className="app">
 		<Header />
 		<Hashtag />
+		<Label content="Mostar eventos" />
 	</main>
 );
 
