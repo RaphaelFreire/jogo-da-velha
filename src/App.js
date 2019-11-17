57de29a45e4326b8419b793f7847dce47c1b4c72
Raphael Freire <raphaelmachadofreire@gmail.com>
2019-11-17 11:07:37 -0300

    criado o iconClose

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import "./App.css";
 import Checkbox from "./objects/Checkbox";
 import About from "./objects/About";
 import AboutLink from "./objects/AboutLink";
+import IconClose from "./objects/IconClose";
 import Header from "./components/Header";
 import Hashtag from "./components/Hastag";
 
 const App = () => (
-	<main className="app">
-		<Header />
-		<Hashtag />
-		<Checkbox
-			id="show"
-			value="show"
-			type="checkbox"
-			content="Mostrar eventos"
-		/>
-		<About>
-			<AboutLink className="-light" />
-		</About>
-	</main>
+  <main className="app">
+    <Header />
+    <Hashtag />
+    <Checkbox
+      id="show"
+      value="show"
+      type="checkbox"
+      content="Mostrar eventos"
+    />
+    <About>
+      <AboutLink className="-light" />
+      <IconClose />
+    </About>
+  </main>
 );
 
 export default App;
